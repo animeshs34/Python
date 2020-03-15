1f40007da4485b086e8aa456b8ab3165f7766293
animeshs34 <46777236+animeshs34@users.noreply.github.com>
2020-03-15 23:54:12 +0530

    Revert "Update Counting_Valleys.py"
    This reverts commit 0cc78794ddb285db56d6fceb169c93308e5aaebb.

--- Counting_Valleys.py
+++ Counting_Valleys.py
@@ -1,11 +1,16 @@
 def countingValleys(s):
     position = 0
-    valley = 0
+    valley = -1
     for i in s:
+        print(valley,i)
+        if position == 0:
+            valley += 1
         if i == "U":
-            position += 1
-            if position == 0:
-                valley += 1
-        else:
-            position -= 1
+            position = position + 1
+        elif i == "D":
+            position = position - 1
+        print(valley)
     return valley
+
+#print(countingValleys("UDDDUDUU"))
+print(countingValleys("UDUUUDUDDD"))
