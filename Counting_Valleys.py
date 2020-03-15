d2595f0c255091fe5b98278401b44d29a4c6adf2
animeshs34 <46777236+animeshs34@users.noreply.github.com>
2020-03-16 00:03:48 +0530

    Update Counting_Valleys.py

--- Counting_Valleys.py
+++ Counting_Valleys.py
@@ -5,8 +5,8 @@ def countingValleys(s):
         if i == "U":
             position += 1
             if position == 0:
+                
                 valley += 1
         else:
             position -= 1
     return valley
-
