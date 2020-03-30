3253c241d499383fe07166fb834d4e6bf7981b46
animeshs34 <46777236+animeshs34@users.noreply.github.com>
2020-03-30 20:04:10 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self):
-        self.data = None
+    def __init__(self, data):
+        self.data = data
         self.next = None
-    def selData(self,data):
+    def setData(self,data):
         self.data = data 
     def getData(self):
         return self.data
@@ -12,6 +12,33 @@ class Node:
         return self.next
     def hasNext(self): 
         return self.next != None 
-    def listLenght(self):
-        current = self.head
     
+class LinkedList(object):
+    def __init__(self):
+        self.lenght = 0
+        self.head = None
+    
+    def addNode(self,node):
+        if self.lenght == 0:
+            self.addBeg(node)
+        else:
+            self.addLast(node)
+
+    def addBeg(self,node):
+        newNode = node
+        newNode.next = self.head
+        self.head = newNode
+        self.lenght += 1
+
+    def addAfterValue(self,data,node):
+        newNode = node
+        currentNode = self.head
+
+        while currentNode.next != None or currentNode.data != data:
+            if currentNode.data == data:
+                newNode.next = currentNode.next
+                currentNode.next = newNode
+                self.lenght += 1
+                return
+            else:
+                currentNode = currentNode.next
